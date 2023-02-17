8e01377a222a51532b9da1f6aba8990825ce9b41
Khuzema <maslam002@citymail.cuny.edu>
2023-02-17 00:09:52 -0500

    Added character length prompt

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var genPass = document.querySelector("#password");
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
-
+  let clickEv = true;
   passwordText.value = password;
+  let charLen = prompt("Character length:");
 
+  function generatePassword() {
+  if (8 <= charLen && charLen <= 128){
+    
+    // alert("You name is " + charLen);
+    console.log("Click: " + clickEv);
+    console.log("Password: " + charLen);
+    console.log("Char length: " + charLen.length);
+    /*How to get input to show in password box 
+    genPass.value = name;  */
+  }
+  else{
+    console.log("Too short.");
+    console.log("Length: " + charLen);
+  }
+  
+}
+var password = generatePassword();
 }
 
 // Add event listener to generate button
